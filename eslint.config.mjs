17b401372156801eeb09bf3adb71d9865251927e
Benjamin Rush <bnjmnrsh@gmail.com>
2026-09-14 20:43:25 +0100

    chore: preserve WordPress external module policy

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -4,6 +4,16 @@ export default [
 	...ranWordPress,
 	{
 		files: ['blocks/**/*.js'],
+		settings: {
+			'import/core-modules': [
+				'@wordpress/api-fetch',
+				'@wordpress/block-editor',
+				'@wordpress/blocks',
+				'@wordpress/components',
+				'@wordpress/element',
+				'@wordpress/i18n',
+			],
+		},
 		rules: {
 			'@wordpress/no-unsafe-wp-apis': 'off',
 		},
