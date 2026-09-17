a7d50e41ff2b7baa51fe3c453285a93651014a14
Benjamin Rush <bnjmnrsh@gmail.com>
2026-09-18 00:43:35 +0100

    test(release): place publisher assertions after extraction

--- tests/reconcile-v1.2.3-contract.sh
+++ tests/reconcile-v1.2.3-contract.sh
@@ -38,8 +38,6 @@ require 'image: mysql:8.0@sha256:7dcddc01f13bab2f15cde676d44d01f61fc9f99fe7785e8
 require 'https://raw.githubusercontent.com/wp-cli/scaffold-command/4a464898bf96f9d5e19e9f04957a702bf9bdc191/templates/install-wp-tests.sh'
 require 'remote_digest="$(jq -r '\''.[0].digest // ""'\'' <<< "$matches")"'
 require 'composer lint:compat'
-test "$(grep -Fc 'resolve_tag_commit() {' <<< "$publisher")" -ge 3
-test "$(grep -Fc 'test "$(resolve_tag_commit "$tag_ref")" = "$RAN_HISTORICAL_COMMIT"' <<< "$publisher")" -ge 3
 
 rebuild=$(awk '
   /^    rebuild:$/ { capture = 1 }
@@ -79,6 +77,9 @@ if grep -Eq 'pnpm install|composer install|scripts/create-release-assets\.sh' <<
   echo 'Write-capable reconciliation publisher must not execute historical build code.' >&2
   exit 1
 fi
+
+test "$(grep -Fc 'resolve_tag_commit() {' <<< "$publisher")" -ge 3
+test "$(grep -Fc 'test "$(resolve_tag_commit "$tag_ref")" = "$RAN_HISTORICAL_COMMIT"' <<< "$publisher")" -ge 3
 
 require 'live_main="$(gh api'
 require 'git/ref/heads/main'
